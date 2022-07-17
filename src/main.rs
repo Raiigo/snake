1dc8f40a805e1c0a5fa6743f44488bd6d8ad3cfc
Nolann <nolann.juet@gmail.com>
2022-07-17 23:36:12 +0200

    Snake head movement works, fixed flashing when moving head

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::process::id;
 use std::time::Duration;
-use bevy::app::App;
-use bevy::DefaultPlugins;
-use bevy::prelude::{Color, Commands, default, Entity, Input, KeyCode, Mut, OrthographicCameraBundle, Query, Res, ResMut, Sprite, SpriteBundle, Time, Timer, Transform, Vec2, Visibility, With, Without};
-use bevy::ecs::component::Component;
-use bevy::math::Vec3;
 
-#[derive(Copy, Clone)]
+use bevy::app::App;
+use bevy::core::Time;
+use bevy::ecs::component::Component;
+use bevy::input::Input;
+use bevy::math::Vec2;
+use bevy::prelude::{
+    Color, Commands, KeyCode, Mut, OrthographicCameraBundle, Query, Res, ResMut, Sprite,
+    SpriteBundle, Timer, Transform, Vec3, With, Entity,
+};
+use bevy::utils::default;
+use bevy::DefaultPlugins;
+
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -16,177 +22,171 @@ enum Direction {
 
 struct UpdateTimer(Timer);
 
-struct SnakeLength(u32);
+struct SnakeLength(usize);
 
-struct SnakePartEntities(Vec<Entity>);
+#[derive(Component)]
+struct SnakeHead {
+    direction: Direction,
+}
 
-struct DirectionBuffer(Vec<Direction>);
-
-struct ApplePos((i32, i32));
-
-struct LastPartInfo(Pos, Direction);
+#[derive(Component)]
+struct SnakePart {
+    index: usize,
+    life_counter: u32,
+}
 
 #[derive(Component)]
 struct Apple;
 
-#[derive(Component)]
-struct SnakeHead;
-
-#[derive(Component, Copy, Clone)]
-struct SnakePartInfo(usize, Direction);
-
-#[derive(Component, Copy, Clone)]
-struct Pos(i32, i32); // x, y
+#[derive(Component, Clone, Copy)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(UpdateTimer(Timer::new(Duration::from_millis(500), true)))
         .insert_resource(SnakeLength(1))
-        .insert_resource(SnakePartEntities(vec![]))
-        .insert_resource(DirectionBuffer(vec![]))
-        .insert_resource(LastPartInfo(Pos(0, 0), Direction::Right))
-        .insert_resource(ApplePos((0, 0)))
         .add_startup_system(setup)
-        .add_system(update_head_direction)
+        .add_system(draw)
         .add_system(update)
+        .add_system(handle_input)
         .run();
 }
 
-fn setup(mut last_part_info: ResMut<LastPartInfo>, mut commands: Commands, mut snake_part_entities: ResMut<SnakePartEntities>, mut direction_buffer: ResMut<DirectionBuffer>, mut apple_pos: ResMut<ApplePos>, mut snake_length: ResMut<SnakeLength>) {
+fn draw(mut drawables: Query<(&Pos, &mut Transform)>) {
+    for (pos, transform) in drawables.iter_mut() {
+        let (pos, mut transform): (&Pos, Mut<Transform>) = (pos, transform);
+        transform.translation = Vec3::new((pos.x * 16) as f32, (pos.y * 16) as f32, 0.0);
+    }
+}
+
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // Add Apple entity
     let x = fastrand::i32(-10..11);
     let y = fastrand::i32(-10..11);
-    commands.spawn().insert_bundle((Apple, Pos(x, y))).insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::YELLOW,
-            custom_size: Some(Vec2::new(16.0, 16.0)),
+    commands
+        .spawn()
+        .insert_bundle((Pos { x, y }, Apple))
+        .insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::YELLOW,
+                custom_size: Some(Vec2::new(16.0, 16.0)),
+                ..default()
+            },
             ..default()
-        },
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
+        });
+
+    // Add Snake head
+    commands
+        .spawn()
+        .insert_bundle((
+            Pos { x: 0, y: 0 },
+            SnakePart {
+                index: 0,
+                life_counter: 1,
+            },
+            SnakeHead {
+                direction: Direction::Right,
+            },
+        ))
+        .insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::RED,
+                custom_size: Some(Vec2::new(16.0, 16.0)),
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
-    apple_pos.0.0 = x;
-    apple_pos.0.1 = y;
-    snake_part_entities.0.push(commands.spawn().insert_bundle((SnakeHead, SnakePartInfo(0, Direction::Right), Pos(0, 0))).insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::RED,
-            custom_size: Some(Vec2::new(16.0, 16.0)),
-            ..default()
-        },
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            ..default()
-        },
-        ..default()
-    }).id()); // Registering head entity
-    direction_buffer.0.push(Direction::Right);
-    snake_part_entities.0.push(commands.spawn().insert_bundle((SnakePartInfo(1, Direction::Right), Pos(-1, 0))).insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::RED,
-            custom_size: Some(Vec2::new(16.0, 16.0)),
-            ..default()
-        },
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            ..default()
-        },
-        ..default()
-    }).id()); // Registering body
-    direction_buffer.0.push(Direction::Right);
-    snake_part_entities.0.push(commands.spawn().insert_bundle((SnakePartInfo(2, Direction::Right), Pos(-2, 0))).insert_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::RED,
-            custom_size: Some(Vec2::new(16.0, 16.0)),
-            ..default()
-        },
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            ..default()
-        },
-        ..default()
-    }).id()); // Registering body
-    direction_buffer.0.push(Direction::Right);
-    last_part_info.0 = Pos(-2, 0);
-    last_part_info.1 = Direction::Right;
-    snake_length.0 = 3;
+        });
 }
 
-fn update_head_direction(mut snake_head: Query<&mut SnakePartInfo, With<SnakeHead>>, keyboard_input: Res<Input<KeyCode>>) {
+fn handle_input(keyboard_input: Res<Input<KeyCode>>, mut snake_head: Query<&mut SnakeHead>) {
     let mut snake_head = snake_head.iter_mut().next().unwrap();
-    if keyboard_input.pressed(KeyCode::Up) { snake_head.1 = Direction::Up; println!("Up pressed"); }
-    if keyboard_input.pressed(KeyCode::Left) { snake_head.1 = Direction::Left; println!("Left pressed"); }
-    if keyboard_input.pressed(KeyCode::Down) { snake_head.1 = Direction::Down; println!("Down pressed"); }
-    if keyboard_input.pressed(KeyCode::Right) { snake_head.1 = Direction::Right; println!("Right pressed"); }
+    if keyboard_input.pressed(KeyCode::Up) {
+        snake_head.direction = Direction::Up
+    }
+    if keyboard_input.pressed(KeyCode::Down) {
+        snake_head.direction = Direction::Down
+    }
+    if keyboard_input.pressed(KeyCode::Left) {
+        snake_head.direction = Direction::Left
+    }
+    if keyboard_input.pressed(KeyCode::Right) {
+        snake_head.direction = Direction::Right
+    }
 }
 
-fn update(mut last_part_info: ResMut<LastPartInfo>, mut snake_part_entities: ResMut<SnakePartEntities>, mut update_timer: ResMut<UpdateTimer>, time: Res<Time>, mut commands: Commands, mut snake_parts: Query<(&mut SnakePartInfo, &mut Pos, &mut Transform)>, mut direction_buffer: ResMut<DirectionBuffer>, mut apple: Query<(&mut Pos, &mut Transform, &mut Apple), Without<SnakePartInfo>>, mut apple_pos: ResMut<ApplePos>, mut snake_length: ResMut<SnakeLength>) {
+// Managing moving the snake and decreasing life_counter
+fn update(
+    mut commands: Commands,
+    mut update_timer: ResMut<UpdateTimer>,
+    time: Res<Time>,
+    snake_head_info: Query<(Entity, &SnakeHead, &Pos)>,
+    mut snake_length: ResMut<SnakeLength>,
+    mut snake_parts: Query<(&mut SnakePart, Entity)>,
+) {
     if update_timer.0.tick(time.delta()).just_finished() {
-        println!("Start Update");
-        let mut just_eat = false;
-        let (a_pos, transform, _): (&Pos, &Transform, &Apple) = apple.iter().next().unwrap();
-        println!("{}, {}", a_pos.0, a_pos.1);
-        println!("{}, {}", apple_pos.0.0, apple_pos.0.1);
-        for (mut snake_part_info, mut pos, mut transform) in snake_parts.iter_mut() {
-            let (mut snake_part_info, mut pos, mut transform): (Mut<SnakePartInfo>, Mut<Pos>, Mut<Transform>) = (snake_part_info, pos, transform);
-            match snake_part_info.1 {
-                Direction::Up => { pos.1 += 1 }
-                Direction::Down => { pos.1 -= 1 }
-                Direction::Left => { pos.0 -= 1 }
-                Direction::Right => { pos.0 += 1 }
-            }
-            println!("{}, {}", pos.0, pos.1);
-            direction_buffer.0[snake_part_info.0] = snake_part_info.1;
-            if snake_part_info.0 == (snake_length.0 - 1) as usize && just_eat == false {
-                last_part_info.0 = Pos(pos.0, pos.1);
-            }
-            if snake_part_info.0 != 0 {
-                snake_part_info.1 = direction_buffer.0[snake_part_info.0 - 1];
-            } else {
-                if pos.0 == apple_pos.0.0 && pos.1 == apple_pos.0.1 {
-                    let x = fastrand::i32(-10..11);
-                    let y = fastrand::i32(-10..11);
-                    let (mut a_pos, mut transform, mut apple): (Mut<Pos>, Mut<Transform>, Mut<Apple>) = apple.iter_mut().next().unwrap();
-                    a_pos.0 = x;
-                    a_pos.1 = y;
-                    apple_pos.0.0 = x;
-                    apple_pos.0.1 = y;
-                    let index = snake_length.0 as usize;
-                    let direction = direction_buffer.0.clone().last().unwrap().clone();
-                    let new_pos = match direction_buffer.0.last().unwrap() {
-                        Direction::Up => { Pos(last_part_info.0.0, last_part_info.0.1) }
-                        Direction::Down => { Pos(last_part_info.0.0, last_part_info.0.1) }
-                        Direction::Left => { Pos(last_part_info.0.0, last_part_info.0.1) }
-                        Direction::Right => { Pos(last_part_info.0.0, last_part_info.0.1) }
-                    };
-                    snake_part_entities.0.push(commands.spawn().insert_bundle((SnakePartInfo(index, direction), new_pos)).insert_bundle(SpriteBundle {
-                        sprite: Sprite {
-                            color: Color::RED,
-                            custom_size: Some(Vec2::new(16.0, 16.0)),
-                            ..default()
-                        },
-                        transform: Transform {
-                            translation: Vec3::new(0.0, 0.0, 0.0),
-                            ..default()
-                        },
-                        ..default()
-                    }).id()); // Registering body
-                    direction_buffer.0.push(direction);
-                    snake_length.0 += 1;
-                    last_part_info.0 = Pos(new_pos.0, new_pos.1);
-                    just_eat = true;
-                }
+        println!("Starting new update sequence");
+        let (entity, snake_head, head_pos) = snake_head_info.iter().next().unwrap();
+
+        // Calculating next pos of the head
+        let next_pos = match snake_head.direction {
+            Direction::Up => Pos {
+                x: head_pos.x,
+                y: head_pos.y + 1,
+            },
+            Direction::Down => Pos {
+                x: head_pos.x,
+                y: head_pos.y - 1,
+            },
+            Direction::Left => Pos {
+                x: head_pos.x - 1,
+                y: head_pos.y,
+            },
+            Direction::Right => Pos {
+                x: head_pos.x + 1,
+                y: head_pos.y,
+            },
+        };
+        // Removing SnakeHead component of the current head before creating a new one
+        commands
+            .entity(entity)
+            .remove::<SnakeHead>();
+
+        // Spawning new head entity
+        commands
+            .spawn()
+            .insert_bundle((next_pos, SnakePart { index: 0, life_counter: snake_length.0 as u32 }, SnakeHead { direction: snake_head.direction }))
+            .insert_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::RED,
+                    custom_size: Some(Vec2::new(16.0, 16.0)),
+                    ..default()
+                },
+                transform: Transform {
+                    translation: Vec3::new((next_pos.x * 16) as f32, (next_pos.y * 16) as f32, 0.0),
+                    ..default()
+                },
+                ..default()
+            });
+
+        // Decrease all life_counter and increasing all index
+        for (mut snake_part, _) in snake_parts.iter_mut() {
+            snake_part.index += 1;
+            snake_part.life_counter -= 1; // Can overflow, we have to remove entity with a life_counter of 0
+        }
+
+        // Remove each part with a life_counter of 0
+        for (snake_part, entity) in snake_parts.iter() {
+            if snake_part.life_counter == 0 {
+                commands
+                    .entity(entity)
+                    .despawn();
             }
         }
     }
-
-    for (snake_part_info, pos, mut transform) in snake_parts.iter_mut() {
-        let (snake_part_info, pos, mut transform): (Mut<SnakePartInfo>, Mut<Pos>, Mut<Transform>) = (snake_part_info, pos, transform);
-        transform.translation = Vec3::new((pos.0 * 16) as f32, (pos.1 * 16) as f32, 0.0);
-    }
-    let (mut pos, mut transform, mut apple): (Mut<Pos>, Mut<Transform>, Mut<Apple>) = apple.iter_mut().next().unwrap();
-    transform.translation = Vec3::new((pos.0 * 16) as f32, (pos.1 * 16) as f32, 0.0);
 }
