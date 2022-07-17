e387d0be004f16eeab9778e3759907289df7741c
Nolann <nolann.juet@gmail.com>
2022-07-17 17:44:42 +0200

    Apple generation BUG : Snake growing is glitched some times

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,9 @@ fn main() {
 
 fn setup(mut last_part_info: ResMut<LastPartInfo>, mut commands: Commands, mut snake_part_entities: ResMut<SnakePartEntities>, mut direction_buffer: ResMut<DirectionBuffer>, mut apple_pos: ResMut<ApplePos>, mut snake_length: ResMut<SnakeLength>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn().insert_bundle((Apple, Pos(10, 10))).insert_bundle(SpriteBundle {
+    let x = fastrand::i32(-10..11);
+    let y = fastrand::i32(-10..11);
+    commands.spawn().insert_bundle((Apple, Pos(x, y))).insert_bundle(SpriteBundle {
         sprite: Sprite {
             color: Color::YELLOW,
             custom_size: Some(Vec2::new(16.0, 16.0)),
@@ -67,8 +69,8 @@ fn setup(mut last_part_info: ResMut<LastPartInfo>, mut commands: Commands, mut s
         },
         ..default()
     });
-    apple_pos.0.0 = 10;
-    apple_pos.0.1 = 10;
+    apple_pos.0.0 = x;
+    apple_pos.0.1 = y;
     snake_part_entities.0.push(commands.spawn().insert_bundle((SnakeHead, SnakePartInfo(0, Direction::Right), Pos(0, 0))).insert_bundle(SpriteBundle {
         sprite: Sprite {
             color: Color::RED,
@@ -124,6 +126,10 @@ fn update_head_direction(mut snake_head: Query<&mut SnakePartInfo, With<SnakeHea
 fn update(mut last_part_info: ResMut<LastPartInfo>, mut snake_part_entities: ResMut<SnakePartEntities>, mut update_timer: ResMut<UpdateTimer>, time: Res<Time>, mut commands: Commands, mut snake_parts: Query<(&mut SnakePartInfo, &mut Pos, &mut Transform)>, mut direction_buffer: ResMut<DirectionBuffer>, mut apple: Query<(&mut Pos, &mut Transform, &mut Apple), Without<SnakePartInfo>>, mut apple_pos: ResMut<ApplePos>, mut snake_length: ResMut<SnakeLength>) {
     if update_timer.0.tick(time.delta()).just_finished() {
         println!("Start Update");
+        let mut just_eat = false;
+        let (a_pos, transform, _): (&Pos, &Transform, &Apple) = apple.iter().next().unwrap();
+        println!("{}, {}", a_pos.0, a_pos.1);
+        println!("{}, {}", apple_pos.0.0, apple_pos.0.1);
         for (mut snake_part_info, mut pos, mut transform) in snake_parts.iter_mut() {
             let (mut snake_part_info, mut pos, mut transform): (Mut<SnakePartInfo>, Mut<Pos>, Mut<Transform>) = (snake_part_info, pos, transform);
             match snake_part_info.1 {
@@ -132,14 +138,22 @@ fn update(mut last_part_info: ResMut<LastPartInfo>, mut snake_part_entities: Res
                 Direction::Left => { pos.0 -= 1 }
                 Direction::Right => { pos.0 += 1 }
             }
+            println!("{}, {}", pos.0, pos.1);
             direction_buffer.0[snake_part_info.0] = snake_part_info.1;
-            if snake_part_info.0 == (snake_length.0 - 1) as usize {
+            if snake_part_info.0 == (snake_length.0 - 1) as usize && just_eat == false {
                 last_part_info.0 = Pos(pos.0, pos.1);
             }
             if snake_part_info.0 != 0 {
                 snake_part_info.1 = direction_buffer.0[snake_part_info.0 - 1];
             } else {
                 if pos.0 == apple_pos.0.0 && pos.1 == apple_pos.0.1 {
+                    let x = fastrand::i32(-10..11);
+                    let y = fastrand::i32(-10..11);
+                    let (mut a_pos, mut transform, mut apple): (Mut<Pos>, Mut<Transform>, Mut<Apple>) = apple.iter_mut().next().unwrap();
+                    a_pos.0 = x;
+                    a_pos.1 = y;
+                    apple_pos.0.0 = x;
+                    apple_pos.0.1 = y;
                     let index = snake_length.0 as usize;
                     let direction = direction_buffer.0.clone().last().unwrap().clone();
                     let new_pos = match direction_buffer.0.last().unwrap() {
@@ -162,11 +176,13 @@ fn update(mut last_part_info: ResMut<LastPartInfo>, mut snake_part_entities: Res
                     }).id()); // Registering body
                     direction_buffer.0.push(direction);
                     snake_length.0 += 1;
+                    last_part_info.0 = Pos(new_pos.0, new_pos.1);
+                    just_eat = true;
                 }
             }
         }
     }
-    println!("{}", snake_parts.iter().count());
+
     for (snake_part_info, pos, mut transform) in snake_parts.iter_mut() {
         let (snake_part_info, pos, mut transform): (Mut<SnakePartInfo>, Mut<Pos>, Mut<Transform>) = (snake_part_info, pos, transform);
         transform.translation = Vec3::new((pos.0 * 16) as f32, (pos.1 * 16) as f32, 0.0);
